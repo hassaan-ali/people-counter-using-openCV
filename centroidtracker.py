020c26e8d91066840d2ceaaa6912118bf4a09210
hassaan-ali <hassaan.ali.nust@gmail.com>
2021-01-31 16:20:18 +0400

    Added functionality to track objects based on centroids and min Eucleadian distance

--- centroidtracker.py
+++ centroidtracker.py
@@ -20,7 +20,42 @@ class CentroidTracker():
     self.nextObjectID +=1
 
   def deregister(self, objectID):
-    del self.object(objectID)
+    del self.objects(objectID)
     del self.disappeared(objectID)
 
- 
+  def update(self, rects):
+    if len(rects) == 0
+      for objectID in list(self.disappeared.keys()):
+        self.disappeared[objectID] +=1
+
+        if self.disappeared(objectID) > self.maxDisappeared:
+          self.deresgister(objectID)
+      return self.objects
+
+    inputCentroids = np.zeros((len(rects), 2), dtype="int")
+
+    for (i, (startX, startY, endX, endY)) in enumerate(rects):
+      cX = int((startX + endX) /2.0)
+      cY = int((startY + endY) /2.0)
+      inputCentroids[i] = (cX, cY)
+      
+    if(len(self.objects) == 0:
+      for i in range(0, len(inputCentroids)):
+        self.register(inputCentroids[i])
+
+    else:
+      objectIDs = list(self.objects.keys())
+      objectCentroids = list(self.objects.values())
+      
+      D = dist.cdist(np.array(objectCentroids), inputCentroids)
+      rows = D.min(axis = 1).argsort()
+      cols = D.argmin(axis = 1)[rows]
+
+      usedRows = set()
+      usedCols = set()
+
+      for (row,col) in zip(rows,cols):
+        if row in usedRows or col in usedCols:
+          continue
+        objectID = objectIDs[row]
+        self.objects[objectID] = inputCentroids[col]
